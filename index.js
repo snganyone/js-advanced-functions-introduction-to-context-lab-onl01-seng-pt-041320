07d9e252d29e706e965183c1634426b583fbf50b
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-15 19:59:54 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+
+function createEmployeeRecord(arr){
+    let emp_record = {};
+    emp_record.firstName = arr[0];
+    emp_record.familyName = arr[1];
+    emp_record.title = arr[2];
+    emp_record.payPerHour = arr[3];
+    emp_record.timeInEvents = [];
+    emp_record.timeOutEvents = [];
+    return emp_record;
+}
